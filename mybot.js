3fa30baaba1a7be6f0bcf9b7129e77352701d306
mrpapersonic <37962225+mrpapersonic@users.noreply.github.com>
2018-12-27 00:05:44 -0500

    added the files for the bot
    files

---
+++ mybot.js
@@ -0,0 +1,237 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("I am ready!");
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!sub")) {
+    message.channel.send("https://www.youtube.com/channel/UCsEF9mQtJiPAjW3FQDUzbGw?sub_confirmation=1");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!kill")) {
+    message.channel.send("kill yourself");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!unsub")) {
+    message.channel.send("from dantdm");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!giveEx-ModBack")) {
+    message.channel.send("hes getting it back in like a few days jeez");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!itsTime")) {
+    message.channel.send("to give ex-mods back to misspapsus");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!stop")) {
+    message.channel.send("@everyone STOPPPP");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!giveEx-ModsBack")) {
+    message.channel.send("hes getting it back in a few days jeez");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!fart")) {
+    message.channel.send("you smell");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!what")) {
+    message.channel.send("https://i.imgur.com/5dgrsL4.png");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!howoldareyou")) {
+    message.channel.send("3");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!help")) {
+    message.channel.send("https://docs.google.com/document/d/1M75bZPmYGA9VPYIwygDLAkZlvD6hWskWfypE9Xd4lXY/edit?usp=sharing");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!succ")) {
+    message.channel.send("https://i.imgur.com/vV2C7Km.png copyright creeft's spicy memes *all* ***lefts*** *reserved*");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!woah")) {
+    message.channel.send("https://i.imgur.com/89AE5Zu.png copyright creeft's spicy memes *all* ***lefts*** *reserved*");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!bandicam")) {
+    message.channel.send("https://www.youtube.com/watch?v=UiRBPZP02Cs");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!duckonscreen")) {
+    message.channel.send("https://i.imgur.com/U0xUi3l.png");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!duckscreendownload")) {
+    message.channel.send("https://i.imgur.com/OHbQ7xh.png");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!alphysismywaifu")) {
+    message.channel.send("https://i.imgur.com/RfV9Y3J.jpg");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!lenny")) {
+    message.channel.send("( ͡° ͜ʖ ͡°)");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!roblox")) {
+    message.channel.send("@everyone dont buy robux");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!lol")) {
+    message.channel.send("@everyone wear your seatbelt");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!technoblade")) {
+    message.channel.send("https://www.youtube.com/user/technothepig?sub_confirmation=1");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!didyoudie")) {
+    message.channel.send("@everyone did you die?");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!ididdie")) {
+    message.channel.send("m'kay");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!ididntdie")) {
+    message.channel.send("https://i.imgur.com/sR0CSGQ.jpg");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!downloadcoder")) {
+    message.channel.send("https://code.visualstudio.com/Download");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!no")) {
+    message.channel.send("https://cdn.drawception.com/images/panels/2015/3-23/BC322qXPhz-4.png");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!monkeysee")) {
+    message.channel.send("monkeydo");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!creeft")) {
+    message.channel.send("UNBAN CREEFT");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!pack")) {
+    message.channel.send("pack: http://www.mediafire.com/file/2w0kbab06250sdp/%21++++++++++++++++++++++++Paper%27s+Mashup+%28EXTRACT%29.zip");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!share")) {
+    message.channel.send("http://bit.ly/MrPaperBot");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!itsTheNutshack")) {
+    message.channel.send("https://www.youtube.com/watch?v=gO_KxGlY62g");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!fireflies")) {
+    message.channel.send("https://www.youtube.com/watch?v=xvBRQNdSqvw");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("aaaaaaaaaaaaaa")) {
+    message.channel.send("aaaaaaaaaaaaa");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!spam")) {
+    message.channel.send(".               hi");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!masturbation")) {
+    message.channel.send("https://i.imgur.com/snRiv5d.png copyright creeft's spicy memes **all** ***lefts*** **reserved**");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!nub")) {
+    message.channel.send("i am a complete nub [yes its a joke]");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("driver - the eye of truth")) {
+    message.channel.send("https://www.youtube.com/watch?v=6UosfX5u14g");
+  }
+});
+
+client.on("message", (message) => {
+  if (message.content.startsWith("!!soop")) {
+    message.channel.send("http://decameron.org/wp-content/uploads/2013/05/SooP2.Logo1_.5.jpg");
+  }
+});
+
+
+client.login("code");
